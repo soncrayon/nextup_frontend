4062546c59dcf9bab152ebf340611538365a84c6
soncrayon <arnold.curtis@gmail.com>
2021-02-03 23:28:08 -0500

    first commit of frontend app

--- src/index.js
+++ src/index.js
@@ -2,16 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Provider} from 'react-redux'
+import {createStore, applyMiddleware, compose, combineReducers} from 'redux'
+import thunk from 'redux-thunk'
+
 import reportWebVitals from './reportWebVitals';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+// const rootReducer = combineReducers({ cards, readings, users })
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
